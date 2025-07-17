303119c6b0699d0e6086f25caa84f8e4a079c3a2
Masaharu Tokimoto <masaharu302@gmail.com>
2025-07-17 23:32:57 +0900

    Fix: env編集

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv.("LINE_CHANNEL_SECRET"))
 
 app = Flask(__name__)
 
