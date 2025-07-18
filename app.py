76cf3f1629dbeefa9f9b4b0a17061eb1682582e0
Masaharu Tokimoto <masaharu302@gmail.com>
2025-07-18 23:25:54 +0900

    修正

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     try:
-        handler.handle(body, sinature)
+        handler.handle(body, signature)
     except Exception as e:
         print("💥 Webhook handler エラー:", e)
     return 'OK'
